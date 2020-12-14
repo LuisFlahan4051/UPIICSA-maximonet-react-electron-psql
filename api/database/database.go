8285e38e60021757220dc7a5d83c388b773479cf
Melendez Bustamante Luis Fernando <luisflahan@localhost.localdomain>
2020-12-14 00:27:46 -0600

    Estructura y base de datos terminad, se han agregado datos. Falta hacer procedures disparadores etc.

---
+++ api/database/database.go
@@ -0,0 +1,36 @@
+package database
+
+import "fmt"
+
+func Connect() {
+	fmt.Println("Hello World!")
+}
+
+/* import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Database struct {
+	Client *mongo.Client
+}
+
+func Connect() *Database {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Connect to MongoDB Succesfully!")
+
+	return &Database{
+		Client: client,
+	}
+} */
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/asticode/go-astikit"
+	"github.com/asticode/go-astilectron"
+)
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	indexTemplate := template.Must(template.ParseFiles("gui/build/index.html"))
+	indexTemplate.Execute(writer, nil)
+}
+
+func main() {
+	portGui := "4051"
+	urlGui := "http://localhost:" + portGui + "/"
+
+	//Crea el servidor de react. FOR BUILD > go build -ldflags "-H windowsgui" -o main.exe
+	go func() {
+		sataticsFiles := http.FileServer(http.Dir("gui/build/static/"))
+		http.Handle("/static/", http.StripPrefix("/static/", sataticsFiles))
+		http.HandleFunc("/", index)
+
+		fmt.Println("Servidor listo y corriendo en el puerto " + portGui + ".")
+		fmt.Println("Ya puede abrir la dirección " + urlGui + " en su navegador.")
+		http.ListenAndServe(":"+portGui, nil)
+	}()
+
+	// Set logger
+	loger := log.New(log.Writer(), log.Prefix(), log.Flags())
+
+	// Create astilectron
+	app, err := astilectron.New(loger, astilectron.Options{
+		AppName:            "simpleApp",
+		AppIconDefaultPath: "logo.png",
+		BaseDirectoryPath:  "dependencies",
+	})
+	if err != nil {
+		loger.Fatal(fmt.Errorf("main: creating astilectron failed: %w", err))
+	}
+	defer app.Close()
+
+	// Handle signals
+	app.HandleSignals()
+
+	// Start
+	if err = app.Start(); err != nil {
+		loger.Fatal(fmt.Errorf("main: starting astilectron failed: %w", err))
+	}
+
+	var loaderWindow *astilectron.Window
+	if loaderWindow, err = app.NewWindow("loader.html", &astilectron.WindowOptions{
+		Center: astikit.BoolPtr(true),
+		Height: astikit.IntPtr(300),
+		Width:  astikit.IntPtr(500),
+		Frame:  astikit.BoolPtr(false),
+	}); err != nil {
+		loger.Fatal(fmt.Errorf("main: new window failed: %w", err))
+	}
+
+	time.Sleep(1 * time.Second)
+	if err = loaderWindow.Create(); err != nil {
+		loger.Fatal(fmt.Errorf("main: creating window failed: %w", err))
+	}
+	time.Sleep(3 * time.Second)
+
+	var window *astilectron.Window
+	if window, err = app.NewWindow(urlGui, &astilectron.WindowOptions{
+		Center: astikit.BoolPtr(true),
+		Height: astikit.IntPtr(700),
+		Width:  astikit.IntPtr(700),
+	}); err != nil {
+		loger.Fatal(fmt.Errorf("main: new window failed: %w", err))
+	}
+	if err = window.Create(); err != nil {
+		loger.Fatal(fmt.Errorf("main: creating window failed: %w", err))
+	}
+
+	loaderWindow.Close()
+	// Blocking pattern
+	app.Wait()
+}
